a8d21372314e8da03bfd3acaf8c9692a37023a98
Rio Arswendo Rachmad <r.arswendo.r@gmail.com>
2023-06-18 14:27:22 +0700

    feat(cracker): added two way cracker function

--- cmd/cracker/cracker.go
+++ cmd/cracker/cracker.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -20,7 +24,22 @@ func createTestBench() {
 	}
 }
 
-func crackFromLow(testCase TestCase) {
+func crackFromUp(testCase TestCase) time.Duration {
+	start := time.Now()
+	guess := "999999"
+	attemptCount := 0
+	for i := 999999; i >= 0; i-- {
+		if guess == testCase.Target {
+			break
+		}
+		attemptCount++
+		guess = fmt.Sprintf("%06d", i)
+	}
+	elapsed := time.Since(start)
+	elapsed = elapsed + (time.Duration(attemptCount/3*5) * time.Second)
+	return elapsed
+}
+func crackFromLow(testCase TestCase) time.Duration {
 	start := time.Now()
 	guess := "000000"
 	attemptCount := 0
@@ -33,18 +52,41 @@ func crackFromLow(testCase TestCase) {
 	}
 	elapsed := time.Since(start)
 	elapsed = elapsed + (time.Duration(attemptCount/3*5) * time.Second)
-	fmt.Printf("(%v) found in: %v\n", testCase.Target, elapsed)
+	return elapsed
+}
+
+var records [][]string = [][]string{
+	{"pin", "crack_from_low", "crack_from_up", "ease_of_guess"},
 }
 
 func main() {
 	createTestBench()
 	var crackers sync.WaitGroup
-	for i := 0; i < len(testBench); i++ {
+	for i := 0; i < 10; i++ {
 		crackers.Add(1)
 		go func(index int) {
 			defer crackers.Done()
-			crackFromLow(testBench[index])
+			fromLow := crackFromLow(testBench[index])
+			fromUp := crackFromUp(testBench[index])
+			easeOfGuess := 0
+			records = append(records, []string{fmt.Sprintf("\"%v\"", testBench[index].Target), strconv.FormatInt(fromLow.Milliseconds(), 10), strconv.FormatInt(fromUp.Milliseconds(), 10), fmt.Sprint(easeOfGuess)})
 		}(i)
 	}
 	crackers.Wait()
+
+	f, err := os.Create("reports.csv")
+	if err != nil {
+		log.Fatalln("failed to open file", err)
+	}
+	defer f.Close()
+
+	w := csv.NewWriter(f)
+	w.Comma = ';'
+	defer w.Flush()
+
+	for _, record := range records {
+		if err := w.Write(record); err != nil {
+			log.Fatalln("error writing record to file", err)
+		}
+	}
 }
