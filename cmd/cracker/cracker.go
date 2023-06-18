b1cca2781b4ac94bceb2c39c6f0f0049e7e71878
Rio Arswendo Rachmad <r.arswendo.r@gmail.com>
2023-06-18 16:05:46 +0700

    feat(cracker): added ease of guess analyzer function

--- cmd/cracker/cracker.go
+++ cmd/cracker/cracker.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
@@ -55,20 +58,69 @@ func crackFromLow(testCase TestCase) time.Duration {
 	return elapsed
 }
 
+// Ease of guess criteria
+// +1 if same number are used more than 2 times
+// +1 for each more repetition of the same number
+// +4 if repetition are used more than once globally
+// +4 if the same number are used consecutively
+// +5 if its on the common password list
+
+func analyzeEaseOfGuess(testCase TestCase) int {
+	splitted := strings.Split(testCase.Target, "")
+	var hashMap map[string]int = map[string]int{}
+	globalRepetition := 0
+	easeOfGuessScore := 0
+
+	for i, numStr := range splitted {
+		hashMap[numStr]++
+		if hashMap[numStr] > 2 {
+			globalRepetition++
+			easeOfGuessScore++
+		}
+		if i > 0 && (numStr == splitted[i-1]) {
+			easeOfGuessScore += 4
+		}
+	}
+	if globalRepetition > 1 {
+		easeOfGuessScore += globalRepetition * 4
+	}
+	if _, found := commonPasswordMap[testCase.Target]; found {
+		easeOfGuessScore += 5
+	}
+	return easeOfGuessScore
+}
+
 var records [][]string = [][]string{
 	{"pin", "crack_from_low", "crack_from_up", "ease_of_guess"},
 }
 
+var commonPasswordMap map[string]int = map[string]int{}
+
+func mapCommonPassword() {
+	jsonFile, err := os.Open("./commonPasswords.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteVal, _ := ioutil.ReadAll(jsonFile)
+	commonPasswords := []string{}
+	json.Unmarshal(byteVal, &commonPasswords)
+	for _, password := range commonPasswords {
+		commonPasswordMap[password] = 1
+	}
+}
+
 func main() {
+	mapCommonPassword()
 	createTestBench()
 	var crackers sync.WaitGroup
-	for i := 0; i < 10; i++ {
+	for i := 0; i < len(testBench); i++ {
 		crackers.Add(1)
 		go func(index int) {
 			defer crackers.Done()
 			fromLow := crackFromLow(testBench[index])
 			fromUp := crackFromUp(testBench[index])
-			easeOfGuess := 0
+			easeOfGuess := analyzeEaseOfGuess(testBench[index])
 			records = append(records, []string{fmt.Sprintf("\"%v\"", testBench[index].Target), strconv.FormatInt(fromLow.Milliseconds(), 10), strconv.FormatInt(fromUp.Milliseconds(), 10), fmt.Sprint(easeOfGuess)})
 		}(i)
 	}
